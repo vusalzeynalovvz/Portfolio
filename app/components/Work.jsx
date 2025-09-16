611912cd20225e682d318cb09e69b14d42c7e4fd
Vüsal <vusalvz@code.edu.az>
2025-09-17 03:12:15 +0400

    Home Finsh

--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -45,7 +45,7 @@ const Work = () => {
       </div>
       <a href="" className="w-max flex items-center justify-center gap-2
       text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto my-20
-      hover:bg-[#fcf4ff] duration-500">
+      hover:bg-black hover:text-white duration-500">
     Show more <Image src={assets.right_arrow_bold} alt="Right arrow" className="w-4"/>
       </a>
     </div>
--- app/page.js
+++ app/page.js
@@ -1,4 +1,6 @@
 import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import Services from "./components/Services";
@@ -12,6 +14,8 @@ export default function Home() {
       <About/>
       <Services/>
       <Work/>
+      <Contact/>
+      <Footer/>
     </>
   );
 }
